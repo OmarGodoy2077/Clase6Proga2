c485939a289b118a9583d85b69438299f75c06c0
Omar Godoy <sgodoym4@miumg.edu.gt>
2023-08-19 11:24:00 -0600

    Initial commit

---
+++ src/main/java/org/example/ArrayListSample.java
@@ -0,0 +1,16 @@
+package org.example.pkg.lista;
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayListSample {
+
+    public void ejemplo1(){
+        List<String> taskList = new ArrayList<>();
+        taskList.add("Sacar al perro");
+        taskList.add("Comprar el pan");
+        taskList.add("hacer la cama");
+        taskList.add("ir a votar");
+
+        System.;
+    }
+
+}
